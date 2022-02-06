20be729007df358a17ac2476f9e5d4fbb16d659a
SHULBHIGUPTA <shulbhi18@gmail.com>
2022-02-06 01:34:56 -0800

    Updates

---
+++ precache-manifest.cb304edc2313411611c3fef29fcf84f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dc90c3601dd06227ed2",
+    "url": "/React-RegistrationForm/static/css/main.4fcd5e26.chunk.css"
+  },
+  {
+    "revision": "2dc90c3601dd06227ed2",
+    "url": "/React-RegistrationForm/static/js/main.71716abf.chunk.js"
+  },
+  {
+    "revision": "527ce2bb6e5e2dcf9b39",
+    "url": "/React-RegistrationForm/static/js/runtime~main.fe7dca7a.js"
+  },
+  {
+    "revision": "a5e9d36056a6ac6e590a",
+    "url": "/React-RegistrationForm/static/js/2.f4f735b6.chunk.js"
+  },
+  {
+    "revision": "8e6429b056aaf767f880c383b02d976b",
+    "url": "/React-RegistrationForm/index.html"
+  }
+];
